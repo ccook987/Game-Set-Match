7e95dca34c014ee377a03f8afcc045cb6e55b4a0
Student <example@example.com>
2018-12-10 14:31:45 -0800

    get results to hide when logged out, work on remove button and limiting it to render on user

--- src/components/PlayerProfileForm.jsx
+++ src/components/PlayerProfileForm.jsx
@@ -32,7 +32,8 @@ class PlayerProfileForm extends Component {
       playername: this.state.playername,
       platform: this.state.platform,
       gameTitle: this.state.gameTitle,
-      platformUserId: this.state.platformUserId
+      platformUserId: this.state.platformUserId,
+      user: this.state.user.displayName || this.state.user.email
     }
     playerRef.push(player);
     this.setState({
@@ -41,6 +42,7 @@ class PlayerProfileForm extends Component {
       gameTitle: '',
       platformUserId: ''
     });
+    console.log(this.state.user.email);                                                                                                                                 
   }
 
   logout() {
@@ -98,46 +100,50 @@ class PlayerProfileForm extends Component {
             <button onClick={this.login}>Log In</button>
           }
           {this.state.user ?
-      <div>
-      <div className='user-profile'>
-        <img src={this.state.user.photoURL} />
-      </div>
-      </div>
-      :
-      <div className='wrapper'>
-        <p>Please log in.</p>
-      </div>
-    }
+              <div>
+                <div className='user-profile'>
+                  <img src={this.state.user.photoURL} />
+                </div>
+                <div className='container'>
+                  <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="playername" placeholder="Name" onChange={this.handleChange} value={this.state.playername}/>
+                    <input type="text" name="platform" placeholder="Platform" onChange={this.handleChange} value={this.state.platform}/>
+                    <input type="text" name="gameTitle" placeholder="Game" onChange={this.handleChange} value={this.state.gameTitle}/>
+                    <input type="text" name="platformUserId" placeholder="Steam/XBOX/PS ID" onChange={this.handleChange} value={this.state.platformUserId}/>
+                    <button type='submit'>Submit</button>
+                  </form>
+                </div>
+                <section className='display-player'>
+                  <div className='wrapper'>
+                    <ul>
+                      {this.state.players.map((player) => {
+                        return (
+                          <li key={player.id}>
+                            <h3>{player.playername}
+                              {player.user === this.state.user.displayName || player.user === this.state.user.email ?
+                  <button onClick={() => this.removePlayer(player.id)}>Remove Player</button> : null}
+                            </h3>
+                            <p>Platform: {player.platform}</p>
+                            <p>Game: {player.gameTitle}</p>
+                            <p>Platform ID: {player.platformUserId}</p>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                </section>
+              </div>
+              :
+              <div className='wrapper'>
+                <p>Please log in.</p>
+              </div>
+            }
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="playername" placeholder="Name" onChange={this.handleChange} value={this.state.playername}/>
-          <input type="text" name="platform" placeholder="Platform" onChange={this.handleChange} value={this.state.platform}/>
-          <input type="text" name="gameTitle" placeholder="Game" onChange={this.handleChange} value={this.state.gameTitle}/>
-          <input type="text" name="platformUserId" placeholder="Steam/XBOX/PS ID" onChange={this.handleChange} value={this.state.platformUserId}/>
-          <button type='submit'>Submit</button>
-        </form>
-        <section className='display-player'>
-          <div className='wrapper'>
-            <ul>
-              {this.state.players.map((player) => {
-                return (
-                  <li key={player.id}>
-                    <h3>{player.playername}
-                      {player.playername === this.state.user.username ?
-                        <button onClick={() => this.removePlayer(player.id)}>Delete Profile</button> : null}
-                    </h3>
-                    <p>Platform: {player.platform}</p>
-                    <p>Game: {player.gameTitle}</p>
-                    <p>Platform ID: {player.platformUserId}</p>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </section>
       </div>
     );
   }
 }
+// {player.playername === this.state.user.username ?
+//   <button onClick={() => this.removePlayer(player.id)}>Delete Profile</button> : null}
 
 export default PlayerProfileForm;
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,15 @@
 import firebase from 'firebase';
 
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
+const config = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: "game-set-match-eb9cb.firebaseapp.com",
+  databaseURL: "https://game-set-match-eb9cb.firebaseio.com",
+  projectId: "game-set-match-eb9cb",
+  storageBucket: "game-set-match-eb9cb.appspot.com",
+  messagingSenderId: "245647734591"
+};
 
 firebase.initializeApp(config);
 
